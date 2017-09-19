17aa53860a60776e3d88a6a989192aecd87f0e40
Kenta Mukai <k_mukai@vivivit.co.jp>
2017-09-20 00:46:30 +0900

    :wrench: add deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
 # config valid only for current version of Capistrano
-lock "3.9.1"
+lock '3.9.1'
 
-set :application, "rails-angular-example"
-set :repo_url, "git@github.com:muk-ai/rails-angular-example.git"
+set :application, 'rails-angular-example'
+set :repo_url, 'https://github.com/muk-ai/rails-angular-example.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -25,6 +27,7 @@ set :repo_url, "git@github.com:muk-ai/rails-angular-example.git"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'logx', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,3 +37,12 @@ set :repo_url, "git@github.com:muk-ai/rails-angular-example.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :keep_releases, 5
+
+# rbenv
+set :rbenv_type, :system # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.4.2'
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w[rake gem bundle ruby rails]
+set :rbenv_roles, :all # default value

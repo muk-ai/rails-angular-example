7afba6fcea4ce8279e72ad1953f16df0c7e73622
Kenta Mukai <k_mukai@vivivit.co.jp>
2017-09-21 01:33:30 +0900

    :sparkles: add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  match '*all', to: 'static_pages#index', via: [:get]
 end

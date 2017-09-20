9aa7e2e15aafcbeef7ab41d40991d51b644bfa69
Kenta Mukai <k_mukai@vivivit.co.jp>
2017-09-21 01:14:18 +0900

    :bug: use append instead of set
    to prevent overwriting variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,5 +45,5 @@ set :rbenv_type, :system # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.2'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w[rake gem bundle ruby rails]
+append :rbenv_map_bins, %[rake gem bundle ruby rails]
 set :rbenv_roles, :all # default value

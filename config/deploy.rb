32669793918ba32409e9305170492d3d14730aa6
Kenta Mukai <k_mukai@vivivit.co.jp>
2017-09-20 23:08:45 +0900

    :wrench: define linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, 'config/secrets.yml.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"

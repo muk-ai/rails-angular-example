d8b67bc2ea6da1070d7b6ddb3925843d5f9cd4c9
Kenta Mukai <k_mukai@vivivit.co.jp>
2018-05-06 00:13:38 +0900

    :sparkles: fetch todos and show it

--- frontend/rails-angular-example/src/app/app.component.ts
+++ frontend/rails-angular-example/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  todos$;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    // this.todos$ = this.http.get('http://localhost:3000/todos');
+    this.todos$ = this.http.get('http://35.200.32.25/todos');
+  }
 }

0c5f96287e07a05a322a4ef3c39b6ff0413d2463
Kenta Mukai <k_mukai@vivivit.co.jp>
2018-05-05 23:33:31 +0900

    :recycle: remove StaticPagesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  match '*all', to: 'static_pages#index', via: [:get]
 end
